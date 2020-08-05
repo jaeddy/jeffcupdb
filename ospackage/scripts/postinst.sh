59aa2375a2c44bb7f44689de19cd3e9f8cd5b8f9
James Eddy <james.a.eddy@gmail.com>
2020-08-05 00:12:14 -0700

    remove debian package scripts

--- ospackage/scripts/postinst.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Reloading daemon"
-systemctl daemon-reload
-echo "Restarting jeffcupdb.service"
-systemctl restart jeffcupdb.service
-echo "Restarting jeffcupdb-update.timer"
-systemctl restart jeffcupdb-update.timer
